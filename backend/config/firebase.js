eff9a8c4feda45f12a9556b6ee001aa50f76ba2e
Cal <calvin.m71@hotmail.com>
2023-09-17 09:37:46 -0700

    firebase

---
+++ backend/config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCxyJovApa2De_B62y5y6eZRyWZcbCQ5lE",
+  authDomain: "stickyboard-5e131.firebaseapp.com",
+  projectId: "stickyboard-5e131",
+  storageBucket: "stickyboard-5e131.appspot.com",
+  messagingSenderId: "377091587302",
+  appId: "1:377091587302:web:6d5956612601d081580f27"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
